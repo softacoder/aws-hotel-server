9b72cef7a8f954ae3074022e12bfd691a04c2aea
Glitch (jan-softa-hotel-server) <none>
2023-08-02 10:20:35 +0000

    👜😁 Updated with Glitch

---
+++ dataModel.js
@@ -0,0 +1,14 @@
+// dataModel.js
+
+const bookingModel = {
+  id: 0,
+  roomId: 0,
+  title: "",
+  firstName: "",
+  surname: "",
+  email: "",
+  checkInDate: "",
+  checkOutDate: "",
+};
+
+module.exports = bookingModel;
