687c5e2e4244e6b0113344ffe0169a5827e3b11a
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-12 13:26:58 +0100

    commit

--- server.js
+++ server.js
@@ -28,13 +28,18 @@ app.get("/bookings/search", (req, res) => {
 
   if (searchDate) {
     if (!moment(searchDate, "YYYY-MM-DD", true).isValid()) {
-      return res.status(400).json({ error: "Invalid date format. Please use 'YYYY-MM-DD' format." });
+      return res.status(400).json({
+        error: "Invalid date format. Please use 'YYYY-MM-DD' format.",
+      });
     }
 
     const bookingsForDate = bookings.filter((booking) => {
       const checkInDate = moment(booking.checkInDate, "YYYY-MM-DD");
       const checkOutDate = moment(booking.checkOutDate, "YYYY-MM-DD");
-      return checkInDate.isSameOrBefore(searchDate, "day") && checkOutDate.isSameOrAfter(searchDate, "day");
+      return (
+        checkInDate.isSameOrBefore(searchDate, "day") &&
+        checkOutDate.isSameOrAfter(searchDate, "day")
+      );
     });
 
     return res.json(bookingsForDate);
@@ -51,7 +56,10 @@ app.get("/bookings/search", (req, res) => {
     return res.json(matchedBookings);
   }
 
-  return res.status(400).json({ error: "Invalid search parameters. Please provide either 'date' or 'term' in the query." });
+  return res.status(400).json({
+    error:
+      "Invalid search parameters. Please provide either 'date' or 'term' in the query.",
+  });
 });
 
 app.get("/bookings/:id", (req, res) => {
@@ -72,7 +80,9 @@ app.post("/bookings", (req, res) => {
 
   for (const key in newBooking) {
     if (!newBooking[key]) {
-      return res.status(400).json({ error: "Invalid booking data. Missing or empty property." });
+      return res
+        .status(400)
+        .json({ error: "Invalid booking data. Missing or empty property." });
     }
   }
 
@@ -83,7 +93,9 @@ app.post("/bookings", (req, res) => {
   const checkInDate = moment(newBooking.checkInDate, "YYYY-MM-DD");
   const checkOutDate = moment(newBooking.checkOutDate, "YYYY-MM-DD");
   if (!checkOutDate.isAfter(checkInDate)) {
-    return res.status(400).json({ error: "Checkout date must be after checkin date." });
+    return res
+      .status(400)
+      .json({ error: "Checkout date must be after checkin date." });
   }
 
   newBooking.id = bookings.length + 1;
@@ -108,11 +120,9 @@ const listener = app.listen(port, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
+// scp -i "AWS-EC2-key-pair.pem" /Users/jansofta/Documents/GitHub/aws-hotel-server/* ec2-user@ec2-18-168-197-240.eu-west-2.compute.amazonaws.com:path/
 
-
-
-
-
+// ssh -i "AWS-EC2-key-pair.pem" ec2-user@ec2-18-168-197-240.eu-west-2.compute.amazonaws.com
 
 // update12. This code work on level 1-5 except bookings id on level1.
 // const express = require("express");
@@ -213,7 +223,6 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
 // update 11. this works on level4 in glitch
 // const express = require("express");
 // const cors = require("cors");
@@ -308,7 +317,6 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
 // update10. this one can handle all request for level 3 using glitch
 // const express = require("express");
 // const cors = require("cors");
@@ -392,11 +400,10 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
 // update9. this version does not find specified booking by Id
 // const express = require("express");
 // const cors = require("cors");
-// const moment = require("moment"); 
+// const moment = require("moment");
 
 // const app = express();
 // const port = process.env.PORT || 3000;
@@ -417,7 +424,7 @@ const listener = app.listen(port, function () {
 // });
 
 // app.get("/bookings/search", (req, res) => {
-//   const searchDate = req.query.date; 
+//   const searchDate = req.query.date;
 
 //   if (!searchDate || !moment(searchDate, "YYYY-MM-DD", true).isValid()) {
 //     return res.status(400).json({ error: "Invalid date format. Please use 'YYYY-MM-DD' format." });
@@ -464,7 +471,6 @@ const listener = app.listen(port, function () {
 // const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
-
 
 // update8 this version do level 1 & 2 and added css
 // const express = require("express");
@@ -534,7 +540,6 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
 // update7 this version works for level 1 & 2
 // const express = require("express");
 // const cors = require("cors");
@@ -600,7 +605,6 @@ const listener = app.listen(port, function () {
 // const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
-
 
 // update6
 // const express = require("express");
@@ -668,8 +672,7 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
-// update5. 
+// update5.
 // const express = require("express");
 // const cors = require("cors");
 // const dataModel = require("./dataModel.js");
@@ -737,7 +740,6 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
 //update4. This version can handle all requirements for level1
 // const express = require("express");
 // const cors = require("cors");
@@ -798,8 +800,6 @@ const listener = app.listen(port, function () {
 // const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
-
-
 
 //update3
 
@@ -867,9 +867,7 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
-
-//update2 I can type in bookings and it displays all bookings 
+//update2 I can type in bookings and it displays all bookings
 
 // const express = require("express");
 // const cors = require("cors");
@@ -909,8 +907,6 @@ const listener = app.listen(port, function () {
 // const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
-
-
 
 // update1 works I can type in bookings and it displays all bookings
 
@@ -953,8 +949,6 @@ const listener = app.listen(port, function () {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
-
-
 //original
 // const express = require("express");
 // const cors = require("cors");
@@ -971,9 +965,7 @@ const listener = app.listen(port, function () {
 //   response.send("Hotel booking server.  Ask for /bookings, etc.");
 // });
 
-
 // // TODO add your routes and helper functions here
-
 
 // const listener = app.listen(process.env.PORT, function() {
 //   console.log("Your app is listening on port " + listener.address().port);
